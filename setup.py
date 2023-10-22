0cd36a2ac3ace86be0d4e6ce33a7150eedae720f
lucidrains <lucidrains@gmail.com>
2023-10-22 10:43:51 -0700

    will leverage LFQ; the binary codes are a perfect fit for genetic / evolutionary algorithms

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'evolutionary-design-molecules',
+  packages = find_packages(exclude=[]),
+  version = '0.0.1',
+  license='MIT',
+  description = 'Evolutionary Design of Molecules',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  long_description_content_type = 'text/markdown',
+  url = 'https://github.com/lucidrains/evolutionary-design-molecules',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'evolutionary algorithms'
+  ],
+  install_requires=[
+    'einops>=0.7.0',
+    'torch>=2.0',
+    'vector-quantize-pytorch>=1.10.0'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
