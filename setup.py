33aeffb9fd2ca592d226a0378f8baa7a049fe5bd
lucidrains <lucidrains@gmail.com>
2023-12-02 07:29:31 -0800

    working version of lfq

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
   install_requires=[
     'einops>=0.7.0',
     'torch>=2.0',
-    'vector-quantize-pytorch>=1.10.0'
+    'vector-quantize-pytorch>=1.11.8'
   ],
   classifiers=[
     'Development Status :: 4 - Beta',
